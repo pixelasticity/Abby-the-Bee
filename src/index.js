223b29d7befe65d28dafc8fa672108035419295f
Alexander Bell <contact@alexanderbell.info>
2022-08-16 12:34:26 -0700

    Swap react-helmet for react-helmet-async
    Avoids a warning

--- src/index.js
+++ src/index.js
@@ -5,15 +5,22 @@ import './index.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
+
+const helmetContext = {};
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <HelmetProvider context={helmetContext}>
+        <App />
+      </HelmetProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+const { helmet } = helmetContext;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
